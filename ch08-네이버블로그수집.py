e419352bdd1cb07dbab5a8db5678efbd16c1347c
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-11-05 12:19:57 +0900

    [네이버블로그수집-속도개선하나] 웹페이지 이미지 띄우기 방지

--- ch08-네이버블로그수집.py
+++ ch08-네이버블로그수집.py
@@ -8,6 +8,10 @@ keyword = input("키워드 입력 >> ")
 opt = webdriver.ChromeOptions()
 opt.add_experimental_option("detach", True)
 
+#. [속도개선] 웹 페이지 이미지 띄우기 방지
+opt.add_argument('--blink-settings=imagesEnabled=false')
+opt.add_experimental_option('prefs', {"profile.managed_default_content_settings.images": 2})
+
 try:
     #. 브라우저 생성
     browser = webdriver.Chrome(options=opt)
@@ -19,7 +23,7 @@ try:
     #. 1 ~ 5 페이지까지 조회
     for page_num in range(1, 6):
         browser.get(f"https://section.blog.naver.com/Search/Post.naver?pageNo={page_num}&rangeType=ALL&orderBy=sim&keyword={keyword}")
-        time.sleep(2)
+        time.sleep(1) #. [속도개선] 이미지 띄우기 방지 후 대기시간 1초 줄임
         link_elements = browser.find_elements(By.CSS_SELECTOR, "a.desc_inner")
         
         for l in link_elements:
@@ -29,7 +33,7 @@ try:
         #. 블로그 페이지 구성이 각각 다른데 모바일은 동일하므로 모바일 페이지로 접속하여 각 블로그 크롤링
         blog_url_for_mobile = blog_url.replace("https://blog.naver", "https://m.blog.naver")
         browser.get(blog_url_for_mobile)
-        time.sleep(2)
+        time.sleep(1) #. [속도개선] 이미지 띄우기 방지 후 대기시간 1초 줄임
         title_element = browser.find_element(By.CSS_SELECTOR, 'div.se-component-content span')        
         content_element = browser.find_element(By.CSS_SELECTOR, 'div.se-main-container')
         title_list.append(title_element.text)
